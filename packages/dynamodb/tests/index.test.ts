e237efb675571ede72065eada6b21cca4339933b
Balázs Orbán <info@balazsorban.com>
2021-05-16 01:47:48 +0200

    test(dynamodb): add tests

---
+++ packages/dynamodb/tests/index.test.ts
@@ -0,0 +1,16 @@
+import { AWS } from "dynamodb"
+import { DynamoDBAdapter } from "../src"
+import runBasicTests from "../../../basic-tests"
+
+const client = new AWS.DynamoDB.DocumentClient()
+const adapter = DynamoDBAdapter(client)
+
+runBasicTests({
+  adapter,
+  db: {
+    async user(id) {},
+    async session(token) {},
+    async account(id) {},
+    async verificationRequest(id) {},
+  },
+})
