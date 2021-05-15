5afbc389eefe1fd0156083eef8137325ba42b658
Balázs Orbán <info@balazsorban.com>
2021-05-16 00:38:16 +0200

    test(fauna): handle NotFound db errors

--- packages/fauna/tests/index.test.ts
+++ packages/fauna/tests/index.test.ts
@@ -15,22 +15,34 @@ const adapter = Adapter({ faunaClient: client })
 
 const q = Fauna.query
 
+/**
+ * FaunaDB throws an error when something is not found in the db,
+ * but the basic tests expect it to be `null`.
+ */
+async function returnNullIfError(fql: any) {
+  try {
+    return await client.query(fql)
+  } catch (error) {
+    if (error.name === "NotFound") return null
+    throw error
+  }
+}
 runBasicTests({
   adapter,
   db: {
     async user(id) {
-      return await client.query(q.Get(q.Ref(q.Collection("users"), id)))
+      return await returnNullIfError(q.Get(q.Ref(q.Collection("users"), id)))
     },
     async session(sessionToken) {
-      return await client.query(
+      return await returnNullIfError(
         q.Get(q.Match(q.Index("session_by_token"), sessionToken))
       )
     },
     async account(id) {
-      return await client.query(q.Get(q.Ref(q.Collection("accounts"), id)))
+      return await returnNullIfError(q.Get(q.Ref(q.Collection("accounts"), id)))
     },
     async verificationRequest(id) {
-      return await client.query(
+      return await returnNullIfError(
         q.Get(q.Ref(q.Collection("verification_requests"), id))
       )
     },
