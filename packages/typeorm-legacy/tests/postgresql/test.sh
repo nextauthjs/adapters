2cb13d6ded44a3ed362356e8731126204ce53da6
ndom91 <yo@ndo.dev>
2021-05-18 01:50:13 +0200

    test(typeorm-legacy): initial postgres support

---
+++ packages/typeorm-legacy/tests/postgresql/test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+PGUSER=nextauth
+PGPORT=5432
+CONTAINER_NAME=next-auth-postgres-test
+
+# Start db
+docker run -d --rm \
+-e POSTGRES_USER=${PGUSER} \
+-e POSTGRES_DB=${PGDATABASE} \
+-e POSTGRES_HOST_AUTH_METHOD=trust \
+--name "${CONTAINER_NAME}" \
+-p ${PGPORT}:5432 \
+-v "$(pwd)/tests/postgresql/schema.sql":/docker-entrypoint-initdb.d/schema.sql \
+postgres:13.3 
+
+echo \"Waiting 20 sec for db to start...\" && sleep 20
+
+# Always stop container, but exit with 1 when tests are failing
+if npx jest tests/postgres --detectOpenHandles --forceExit;then
+    # docker stop "${CONTAINER_NAME}"
+    echo "good"
+else
+    # docker stop "${CONTAINER_NAME}" && exit 1
+    echo "bad"
+fi
