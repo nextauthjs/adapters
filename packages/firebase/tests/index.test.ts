b0cd7a9419b354e5d1904ff26d8b24c3aefb8f73
ndom91 <yo@ndo.dev>
2021-05-21 01:08:53 +0200

    chore(firebase): update tests to current standard

--- packages/firebase/tests/index.test.ts
+++ packages/firebase/tests/index.test.ts
@@ -1,153 +1,197 @@
-import * as admin from "firebase-admin";
-import { exposeMockFirebaseAdminApp } from 'ts-mock-firebase';
-import Adapter, {
-    IUser,
-    ISession,
-    IVerificationRequest
-} from "../src";
+import * as admin from "firebase-admin"
+import { exposeMockFirebaseAdminApp } from "ts-mock-firebase"
+import Adapter, { IUser, ISession, IVerificationRequest } from "../src"
+import type { AppOptions } from "next-auth/internals"
+import Providers from "next-auth/providers"
 
-const app = admin.initializeApp({});
-const mocked = exposeMockFirebaseAdminApp(app);
+const app = admin.initializeApp({})
+const mocked = exposeMockFirebaseAdminApp(app)
 const firebaseAdapter = Adapter({
-    firestoreAdmin: admin.firestore(),
-    usersCollection: "users",
-    accountsCollection: "accounts",
-    sessionsCollection: "sessions",
-    verificationRequestsCollection: "verificationRequests"
-});
+  firestoreAdmin: admin.firestore(),
+  usersCollection: "users",
+  accountsCollection: "accounts",
+  sessionsCollection: "sessions",
+  verificationRequestsCollection: "verificationRequests",
+})
 
-let session: ISession | null = null;
-let user: IUser | null = null;
-let verificationRequest: IVerificationRequest | null = null;
-const SECRET = "secret";
-const TOKEN = "secret";
+let session: ISession | null = null
+let user: IUser | null = null
+let verificationRequest: IVerificationRequest | null = null
+
+const SECRET = "secret"
+const TOKEN = "secret"
+
+const appOptions: AppOptions = {
+  action: "signin",
+  basePath: "",
+  baseUrl: "",
+  callbacks: {},
+  cookies: {},
+  debug: false,
+  events: {},
+  jwt: {},
+  theme: "auto",
+  logger: {
+    debug: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn(),
+  } as const,
+  pages: {},
+  providers: [],
+  secret: "",
+  session: {
+    jwt: false,
+    maxAge: 60 * 60 * 24 * 30,
+    updateAge: 60 * 60 * 24,
+  },
+  adapter: firebaseAdapter as any,
+}
+
+const sendVerificationRequestMock = jest.fn()
+
+const emailProvider = {
+  ...Providers.Email({
+    sendVerificationRequest: sendVerificationRequestMock,
+  }),
+} as any
+
 describe("adapter functions", () => {
-    // User
-    test("createUser", async () => {
-       const nextDocId = "mocked_user_id";
-       mocked.firestore().mocker.setNextDocumentIds([nextDocId]);
-       let adapter = await firebaseAdapter.getAdapter();
+  afterAll(async () => {
+    // TODO: figure out how mocked or docker firebase works
+    await mocked.$disconnect()
+  })
+  // User
+  test("createUser", async () => {
+    const adapter = await firebaseAdapter.getAdapter(appOptions)
 
-        user = await adapter.createUser({
-            email: "test@next-auth.com",
-            name: "test",
-            image: "https://",
-        } as any);
-        if (!user) {
-            throw new Error('user is bad');
-        }
-        expect(user.id).toMatchInlineSnapshot(`"${nextDocId}"`);
-        expect(user.email).toMatchInlineSnapshot(`"test@next-auth.com"`);
-        expect(user.name).toMatchInlineSnapshot(`"test"`);
-        expect(user.image).toMatchInlineSnapshot(`"https://"`);
-    });
-    test("updateUser", async () => {
-        let adapter = await firebaseAdapter.getAdapter();
-        if (!user) throw new Error("No User Available");
+    user = await adapter.createUser({
+      email: "test@next-auth.com",
+      name: "test",
+      image: "https://",
+    } as any)
 
-        user = await adapter.updateUser({
-            id: user.id,
-            name: "Changed",
-        } as any);
-        expect(user!.name).toEqual("Changed");
-    });
-    // Sessions
-    test("createSession", async () => {
-        let adapter = await firebaseAdapter.getAdapter();
-        session = await adapter.createSession({
-            id: 1,
-        } as any);
+    expect(user.email).toMatchInlineSnapshot(`"test@next-auth.com"`)
+    expect(user.name).toMatchInlineSnapshot(`"test"`)
+    expect(user.image).toMatchInlineSnapshot(`"https://"`)
+  })
+  test("updateUser", async () => {
+    const adapter = await firebaseAdapter.getAdapter(appOptions)
+    if (!user) throw new Error("No User Available")
 
-        expect(session.sessionToken.length).toMatchInlineSnapshot(`64`);
-        expect(session.accessToken.length).toMatchInlineSnapshot(`64`);
-        expect(session.userId).toMatchInlineSnapshot(`1`);
-    });
+    user = await adapter.updateUser({
+      id: user.id,
+      name: "Changed",
+    } as any)
+    expect(user?.name).toEqual("Changed")
+  })
+  // Sessions
+  test("createSession", async () => {
+    const adapter = await firebaseAdapter.getAdapter(appOptions)
+    if (!user) throw new Error("No User Available")
+    session = await adapter.createSession({
+      id: user.id,
+    } as any)
 
-    test("getSession", async () => {
-        let adapter = await firebaseAdapter.getAdapter();
-        if (!session) throw new Error("No Session Available");
+    expect(session.sessionToken.length).toMatchInlineSnapshot(`64`)
+    expect(session.accessToken.length).toMatchInlineSnapshot(`64`)
+  })
 
-        const result = (await adapter.getSession(session.sessionToken)) as ISession;
+  test("getSession", async () => {
+    const adapter = await firebaseAdapter.getAdapter(appOptions)
+    if (!session) throw new Error("No Session Available")
 
-        expect(result.sessionToken).toEqual(session.sessionToken);
-        expect(result.accessToken).toEqual(session.accessToken);
-        expect(result.userId).toMatchInlineSnapshot(`1`);
-    });
-    test("updateSession", async () => {
-        const maxAge = 30 * 24 * 60 * 60;
-        let adapter = await firebaseAdapter.getAdapter({
-            session: { maxAge }
-        });
-        if (!session) throw new Error("No Session Available");
+    const result = await adapter.getSession(session.sessionToken)
 
-        const expiresRequested = new Date(2070, 1);
-        const expiresExpected = Date.now() + (maxAge * 1000);
-        session = (await adapter.updateSession(
-            {
-                ...session,
-                expires: expiresRequested,
-            },
-            true
-        )) as ISession;
+    expect(result?.sessionToken).toEqual(session.sessionToken)
+    expect(result?.accessToken).toEqual(session.accessToken)
+  })
+  test("updateSession", async () => {
+    const adapter = await firebaseAdapter.getAdapter(appOptions)
+    if (!session) throw new Error("No Session Available")
 
-        const difference = Math.abs(session.expires.getTime() - expiresExpected);
-        expect(difference).toBeLessThan(5);
-    });
+    const expires = new Date(2070, 1)
+    session = await adapter.updateSession(
+      {
+        accessToken: "e.e.e",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        userId: "userId",
+        expires,
+        id: session.id,
+        sessionToken: session.sessionToken,
+      },
+      true
+    )
+    if (!session) throw new Error("No Session Updated")
 
-    test("deleteSession", async () => {
-        let adapter = await firebaseAdapter.getAdapter();
-        if (!session) throw new Error("No Session Available");
-        await adapter.deleteSession(session.sessionToken);
-    });
-    // VerificationRequests
-    test("createVerificationRequest", async () => {
-        const nextDocId = "mocked_verification_request_id";
-        mocked.firestore().mocker.setNextDocumentIds([nextDocId]);
-        let adapter = await firebaseAdapter.getAdapter();
-        verificationRequest = await adapter.createVerificationRequest(
-            "any",
-            "https://some.where",
-            TOKEN,
-            SECRET,
-            {
-                maxAge: 90,
-                sendVerificationRequest: async (request: any) => {
+    // Using default maxAge, which is 30 days
+    const thirtyDaysFromNow = new Date()
+    thirtyDaysFromNow.setDate(thirtyDaysFromNow.getDate() + 30)
+    expect(
+      Math.abs(session.expires.getTime() - thirtyDaysFromNow.getTime())
+    ).toBeLessThan(1000)
+  })
 
-                },
-            } as any
-        );
-        expect(verificationRequest.id).toMatchInlineSnapshot(`"${nextDocId}"`);
-        expect(verificationRequest.identifier).toEqual("any");
-    });
-    test("getVerificationRequest", async () => {
-        let adapter = await firebaseAdapter.getAdapter();
-        if (!verificationRequest)
-            throw new Error("No Verification Request Available");
+  test("deleteSession", async () => {
+    const adapter = await firebaseAdapter.getAdapter(appOptions)
+    if (!session) throw new Error("No Session Available")
+    await adapter.deleteSession(session.sessionToken)
+    const result = await prisma.session.findUnique({
+      where: { sessionToken: session.sessionToken },
+    })
+    expect(result).toBe(null)
+  })
+  // VerificationRequests
+  test("createVerificationRequest", async () => {
+    const adapter = await firebaseAdapter.getAdapter(appOptions)
+    const identifier = "any"
+    await adapter.createVerificationRequest?.(
+      identifier,
+      "https://some.where",
+      TOKEN,
+      SECRET,
+      emailProvider
+    )
+    const result = await prisma.verificationRequest.findMany({
+      where: { identifier },
+    })
+    verificationRequest = result?.[0]
+    expect(verificationRequest.identifier).toEqual(identifier)
+    expect(sendVerificationRequestMock).toBeCalledTimes(1)
+  })
+  test("getVerificationRequest", async () => {
+    const adapter = await firebaseAdapter.getAdapter(appOptions)
+    if (!verificationRequest)
+      throw new Error("No Verification Request Available")
 
-        const result = await adapter.getVerificationRequest(
-            verificationRequest.identifier,
-            TOKEN,
-            SECRET,
-            "provider"
-        );
-        expect(result?.token).toEqual(verificationRequest.token);
-    });
-    test("deleteVerificationRequest", async () => {
-        let adapter = await firebaseAdapter.getAdapter();
-        if (!verificationRequest)
-            throw new Error("No Verification Request Available");
-        await adapter.deleteVerificationRequest(
-            verificationRequest.identifier,
-            TOKEN,
-            SECRET,
-            "provider"
-        );
-    });
+    const result = await adapter.getVerificationRequest?.(
+      verificationRequest.identifier,
+      TOKEN,
+      SECRET,
+      emailProvider
+    )
+    expect(result?.token).toEqual(verificationRequest.token)
+  })
+  test("deleteVerificationRequest", async () => {
+    const adapter = await firebaseAdapter.getAdapter(appOptions)
+    if (!verificationRequest)
+      throw new Error("No Verification Request Available")
+    await adapter.deleteVerificationRequest?.(
+      verificationRequest.identifier,
+      TOKEN,
+      SECRET,
+      emailProvider
+    )
+    const result = await prisma.verificationRequest.findUnique({
+      where: { token: TOKEN },
+    })
+    expect(result).toEqual(null)
+  })
 
-    // test('linkAccount', async () => {
-    //   let adapter = await firebaseAdapter.getAdapter();
-    //   const result = await adapter.linkAccount()
-    //   expect(result).toMatchInlineSnapshot()
+  // test('linkAccount', async () => {
+  //   let adapter = await firebaseAdapter.getAdapter();
+  //   const result = await adapter.linkAccount()
+  //   expect(result).toMatchInlineSnapshot()
 
-    // })
-});
+  // })
+})
