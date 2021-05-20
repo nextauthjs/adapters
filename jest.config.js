192a3b4e9a239f4ffefc64a332d7b7fb46f75ff6
Balázs Orbán <info@balazsorban.com>
2021-05-20 20:11:56 +0200

    chore: prepare for Codecov

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   modulePathIgnorePatterns: ["dist/", "node_modules/"],
+  // TODO: Set-up with Codecov https://about.codecov.io/
+  // collectCoverage: true,
+  // coverageReporters: ["json", "html"],
 }
