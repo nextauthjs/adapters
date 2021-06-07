28b80209ca66231e1ad6be9fcecc8843f593a4c4
Rafay Khan <69214549+EXIT-ALAMERY@users.noreply.github.com>
2021-06-07 14:38:32 +0500

    Update packages/sequelize/example/db/models/index.js
    Co-authored-by: Luke Jones <luke-j@users.noreply.github.com>

--- packages/sequelize/example/db/models/index.js
+++ packages/sequelize/example/db/models/index.js
@@ -5,8 +5,6 @@ import {
   userModel, accountModel, sessionModel, verificationRequestModel 
 } from '@next-auth/sequelize-adapter';
 
-const path = require('path');
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
