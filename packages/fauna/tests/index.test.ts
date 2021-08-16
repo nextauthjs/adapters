d272b41c620570917da01675d80682ee7ee3c870
Balázs Orbán <info@balazsorban.com>
2021-08-16 22:40:56 +0200

    feat(fauna): use new Adapter API (#182)
    * feat(fauna): update schema files
    
    * feat(fauna): rewrite to TypeScript
    
    * test(fauna): update tests
    
    * refactor(fauna): simplify codebase
    
    * chore(deps): upgrade `next-auth`
    
    * refactor(fauna): simplify code
    
    * fix(fauna): finish `deleteUser` method
    
    * refactor(fauna): rename indexes
    
    * refactor(fauna): rename to schema.fql
    
    * refactor(fauna): rename index
    
    * refactor(fauna): use Do+ForEach instead of Let+Map
    
    * test(fauna): fix tests
    
    * fix(fauna): splice schema to collections and indexes
    
    * docs(fauna): minor corrections

--- packages/fauna/tests/index.test.ts
+++ packages/fauna/tests/index.test.ts
@@ -1,114 +1,35 @@
-// eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
-// @ts-ignore
-import { FaunaAdapter } from "../src"
+import { collections, FaunaAdapter, format, indexes, query } from "../src"
 import { runBasicTests } from "../../../basic-tests"
-import * as Fauna from "faunadb"
+import { Client as FaunaClient, Get, Match, Ref } from "faunadb"
 
-const client = new Fauna.Client({
+const client = new FaunaClient({
   secret: "secret",
   scheme: "http",
   domain: "localhost",
   port: 8443,
 })
 
-const adapter = FaunaAdapter({ faunaClient: client })
+const q = query(client, format.from)
 
-const q = Fauna.query
-
-/**
- * FaunaDB throws an error when something is not found in the db,
- * but the basic tests expect it to be `null`.
- */
-async function returnNullIfError(fql: any) {
-  try {
-    return await client.query(fql)
-  } catch (error) {
-    if (error.name === "NotFound") return null
-    throw error
-  }
-}
 runBasicTests({
-  adapter,
+  adapter: FaunaAdapter(client),
   db: {
-    async user(id) {
-      const response = await returnNullIfError(
-        q.Get(q.Ref(q.Collection("users"), id))
-      )
+    disconnect: async () => await client.close({ force: true }),
+    user: async (id) => await q(Get(Ref(collections.Users, id))),
+    session: async (sessionToken) =>
+      await q(Get(Match(indexes.SessionByToken, sessionToken))),
+    async account({ provider, providerAccountId }) {
+      const key = [provider, providerAccountId]
+      const ref = Match(indexes.AccountByProviderAndProviderAccountId, key)
+      return await q(Get(ref))
+    },
+    async verificationToken({ identifier, token }) {
+      const key = [identifier, token]
+      const ref = Match(indexes.VerificationTokenByIdentifierAndToken, key)
+      const verificationToken = await q(Get(ref))
       // @ts-expect-error
-      const user = response.data
-      user.createdAt = new Date(user.createdAt.value)
-      user.updatedAt = new Date(user.updatedAt.value)
-      // @ts-expect-error
-      user.id = response.ref.id
-      return user
-    },
-    async session(sessionToken) {
-      return await returnNullIfError(
-        q.Get(q.Match(q.Index("session_by_token"), sessionToken))
-      )
-    },
-    async expireSession(sessionToken, expires) {
-      await client.query(
-        q.Update(
-          q.Select(
-            ["ref"],
-            q.Get(q.Match(q.Index("session_by_token"), sessionToken))
-          ),
-          { data: { expires: q.Time(expires.toISOString()) } }
-        )
-      )
-    },
-    async account(providerId, providerAccountId) {
-      const response = await returnNullIfError(
-        q.Get(
-          q.Match(q.Index("account_by_provider_account_id"), [
-            providerId,
-            providerAccountId,
-          ])
-        )
-      )
-
-      if (!response) {
-        return null
-      }
-
-      // @ts-expect-error
-      const account = response.data
-      account.createdAt = new Date(account.createdAt.value)
-      account.updatedAt = new Date(account.updatedAt.value)
-      account.accessTokenExpires = null
-
-      return account
-    },
-    async verificationRequest(identifier, hashedToken) {
-      const response: any = await returnNullIfError(
-        q.Let(
-          {
-            ref: q.Match(
-              q.Index("verification_request_by_token_and_identifier"),
-              [hashedToken, identifier]
-            ),
-          },
-          q.If(
-            q.Exists(q.Var("ref")),
-            {
-              ref: q.Var("ref"),
-              request: q.Select("data", q.Get(q.Var("ref"))),
-            },
-            null
-          )
-        )
-      )
-      if (response) {
-        const { request: verificationRequest } = response
-        return {
-          ...verificationRequest,
-          expires: new Date(verificationRequest.expires.value),
-          createdAt: new Date(verificationRequest.createdAt.value),
-          updatedAt: new Date(verificationRequest.updatedAt.value),
-        }
-      }
-      return null
+      if (verificationToken) delete verificationToken.id
+      return verificationToken
     },
   },
 })
