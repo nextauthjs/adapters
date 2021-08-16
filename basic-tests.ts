3baee8336063e3a3d2fad41a309cfd31d647ffce
Balázs Orbán <info@balazsorban.com>
2021-08-16 02:59:50 +0200

    test(fauna): update tests

--- basic-tests.ts
+++ basic-tests.ts
@@ -52,6 +52,7 @@ export function runBasicTests(options: TestOptions) {
     email: "fill@murray.com",
     image: "https://www.fillmurray.com/460/300",
     name: "Fill Murray",
+    emailVerified: new Date(),
   }
 
   const session: any = {
@@ -74,232 +75,228 @@ export function runBasicTests(options: TestOptions) {
     session_state: randomUUID(),
   }
 
-  describe("Required (User, Account, Session) methods", () => {
-    test("exist", () => {
-      const requiredMethods = [
-        "createUser",
-        "getUser",
-        "getUserByEmail",
-        "getUserByAccount",
-        "updateUser",
-        "linkAccount",
-        "createSession",
-        "getSessionAndUser",
-        "updateSession",
-        "deleteSession",
-      ]
-      requiredMethods.forEach((method) => {
-        expect(adapter).toHaveProperty(method)
-      })
-    })
+  // All adapters must define these methods
 
-    test("createUser", async () => {
-      const { id } = await adapter.createUser(user)
-      const dbUser = await db.user(id)
-      expect(dbUser).toEqual({
-        ...user,
-        emailVerified: null,
-        id,
-      })
-      user = dbUser
-      session.userId = dbUser.id
-      account.userId = dbUser.id
-    })
-
-    test("getUser", async () => {
-      expect(await adapter.getUser("non-existent-user-id")).toBeNull()
-      expect(await adapter.getUser(user.id)).toEqual(user)
-    })
-
-    test("getUserByEmail", async () => {
-      expect(await adapter.getUserByEmail("non-existent-email")).toBeNull()
-      expect(await adapter.getUserByEmail(user.email)).toEqual(user)
-    })
-
-    test("createSession", async () => {
-      const { sessionToken } = await adapter.createSession(session)
-      const dbSession = await db.session(sessionToken)
-
-      expect(dbSession).toEqual({ ...session, id: dbSession.id })
-      session.userId = dbSession.userId
-      session.id = dbSession.id
-    })
-
-    test("getSessionAndUser", async () => {
-      let sessionAndUser = await adapter.getSessionAndUser("invalid-token")
-      expect(sessionAndUser).toBeNull()
-
-      sessionAndUser = await adapter.getSessionAndUser(session.sessionToken)
-      if (!sessionAndUser) {
-        throw new Error("Session and User was not found, but they should exist")
-      }
-      expect(sessionAndUser).toEqual({
-        user,
-        session,
-      })
-    })
-
-    test("updateUser", async () => {
-      const newName = "Updated Name"
-      await adapter.updateUser({ id: user.id, name: newName })
-
-      const dbUser = await db.user(user.id)
-      expect(dbUser.name).toBe(newName)
-      user.name = newName
-    })
-
-    test("updateSession", async () => {
-      let dbSession = await db.session(session.sessionToken)
-      expect(dbSession.expires.valueOf()).not.toBe(ONE_MONTH_FROM_NOW.valueOf())
-
-      await adapter.updateSession({
-        sessionToken: session.sessionToken,
-        expires: ONE_MONTH_FROM_NOW,
-      })
-      dbSession = await db.session(session.sessionToken)
-      expect(dbSession.expires.valueOf()).toBe(ONE_MONTH_FROM_NOW.valueOf())
-    })
-
-    test("linkAccount", async () => {
-      await adapter.linkAccount(account)
-      const dbAccount = await db.account({
-        provider: account.provider,
-        providerAccountId: account.providerAccountId,
-      })
-      expect(dbAccount).toEqual({ ...account, id: dbAccount.id })
-    })
-
-    test("getUserByAccount", async () => {
-      let userByAccount = await adapter.getUserByAccount({
-        provider: "invalid-provider",
-        providerAccountId: "invalid-provider-account-id",
-      })
-      expect(userByAccount).toBeNull()
-
-      userByAccount = await adapter.getUserByAccount({
-        provider: account.provider,
-        providerAccountId: account.providerAccountId,
-      })
-      expect(userByAccount).toEqual(user)
-    })
-
-    test("deleteSession", async () => {
-      await adapter.deleteSession(session.sessionToken)
-      const dbSession = await db.session(session.sessionToken)
-      expect(dbSession).toBeNull()
+  test("Required (User, Account, Session) methods exist", () => {
+    const requiredMethods = [
+      "createUser",
+      "getUser",
+      "getUserByEmail",
+      "getUserByAccount",
+      "updateUser",
+      "linkAccount",
+      "createSession",
+      "getSessionAndUser",
+      "updateSession",
+      "deleteSession",
+    ]
+    requiredMethods.forEach((method) => {
+      expect(adapter).toHaveProperty(method)
     })
   })
 
-  describe("Optional (Verification Token) methods", () => {
-    test("exist", () => {
-      const requiredMethods = [
-        "createVerificationToken",
-        "useVerificationToken",
-      ]
-      requiredMethods.forEach((method) => {
-        expect(adapter).toHaveProperty(method)
-      })
+  test("createUser", async () => {
+    const { id } = await adapter.createUser(user)
+    const dbUser = await db.user(id)
+    expect(dbUser).toEqual({
+      ...user,
+      id,
     })
+    user = dbUser
+    session.userId = dbUser.id
+    account.userId = dbUser.id
+  })
 
-    test("createVerificationToken", async () => {
-      const identifier = "info@example.com"
-      const token = randomUUID()
-      const hashedToken = hashToken(token)
+  test("getUser", async () => {
+    expect(await adapter.getUser("non-existent-user-id")).toBeNull()
+    expect(await adapter.getUser(user.id)).toEqual(user)
+  })
 
-      const verificationToken = {
-        token: hashedToken,
-        identifier,
-        expires: FIFTEEN_MINUTES_FROM_NOW,
-      }
-      await adapter.createVerificationToken?.(verificationToken)
+  test("getUserByEmail", async () => {
+    expect(await adapter.getUserByEmail("non-existent-email")).toBeNull()
+    expect(await adapter.getUserByEmail(user.email)).toEqual(user)
+  })
 
-      const dbVerificationToken = await db.verificationToken({
-        token: hashedToken,
-        identifier,
-      })
+  test("createSession", async () => {
+    const { sessionToken } = await adapter.createSession(session)
+    const dbSession = await db.session(sessionToken)
 
-      expect(dbVerificationToken).toEqual(verificationToken)
-    })
+    expect(dbSession).toEqual({ ...session, id: dbSession.id })
+    session.userId = dbSession.userId
+    session.id = dbSession.id
+  })
 
-    test("useVerificationToken", async () => {
-      const identifier = "info@example.com"
-      const token = randomUUID()
-      const hashedToken = hashToken(token)
-      const FIFTEEN_MINUTES_FROM_NOW = new Date(Date.now() + 15 * 60 * 1000)
-      const verificationToken = {
-        token: hashedToken,
-        identifier,
-        expires: FIFTEEN_MINUTES_FROM_NOW,
-      }
-      await adapter.createVerificationToken?.(verificationToken)
+  test("getSessionAndUser", async () => {
+    let sessionAndUser = await adapter.getSessionAndUser("invalid-token")
+    expect(sessionAndUser).toBeNull()
 
-      const dbVerificationToken1 = await adapter.useVerificationToken?.({
-        identifier,
-        token: hashedToken,
-      })
-
-      if (!dbVerificationToken1) {
-        throw new Error("Verification Token was not found, but it should exist")
-      }
-
-      expect(dbVerificationToken1).toEqual(verificationToken)
-
-      const dbVerificationToken2 = await adapter.useVerificationToken?.({
-        identifier,
-        token: hashedToken,
-      })
-
-      expect(dbVerificationToken2).toBeNull()
+    sessionAndUser = await adapter.getSessionAndUser(session.sessionToken)
+    if (!sessionAndUser) {
+      throw new Error("Session and User was not found, but they should exist")
+    }
+    expect(sessionAndUser).toEqual({
+      user,
+      session,
     })
   })
 
-  describe("Future methods", () => {
-    // These methods are not yet invoked in the core.
-    test("exist", () => {
-      const requiredMethods = ["unlinkAccount", "deleteUser"]
-      requiredMethods.forEach((method) => {
-        expect(adapter).toHaveProperty(method)
-      })
+  test("updateUser", async () => {
+    const newName = "Updated Name"
+    await adapter.updateUser({ id: user.id, name: newName })
+
+    const dbUser = await db.user(user.id)
+    expect(dbUser.name).toBe(newName)
+    user.name = newName
+  })
+
+  test("updateSession", async () => {
+    let dbSession = await db.session(session.sessionToken)
+
+    expect(dbSession.expires.valueOf()).not.toBe(ONE_MONTH_FROM_NOW.valueOf())
+
+    await adapter.updateSession({
+      sessionToken: session.sessionToken,
+      expires: ONE_MONTH_FROM_NOW,
     })
 
-    test("unlinkAccount", async () => {
-      let dbAccount = await db.account({
-        provider: account.provider,
-        providerAccountId: account.providerAccountId,
-      })
-      expect(dbAccount).toEqual({ ...account, id: dbAccount.id })
+    dbSession = await db.session(session.sessionToken)
+    expect(dbSession.expires.valueOf()).toBe(ONE_MONTH_FROM_NOW.valueOf())
+  })
 
-      await adapter.unlinkAccount?.({
-        provider: account.provider,
-        providerAccountId: account.providerAccountId,
-      })
-      dbAccount = await db.account({
-        provider: account.provider,
-        providerAccountId: account.providerAccountId,
-      })
-      expect(dbAccount).toBeNull()
+  test("linkAccount", async () => {
+    await adapter.linkAccount(account)
+    const dbAccount = await db.account({
+      provider: account.provider,
+      providerAccountId: account.providerAccountId,
+    })
+    expect(dbAccount).toEqual({ ...account, id: dbAccount.id })
+  })
 
-      // Re-add account so we can test `deleteUser` under
-      await adapter.linkAccount(account)
+  test("getUserByAccount", async () => {
+    let userByAccount = await adapter.getUserByAccount({
+      provider: "invalid-provider",
+      providerAccountId: "invalid-provider-account-id",
+    })
+    expect(userByAccount).toBeNull()
+
+    userByAccount = await adapter.getUserByAccount({
+      provider: account.provider,
+      providerAccountId: account.providerAccountId,
+    })
+    expect(userByAccount).toEqual(user)
+  })
+
+  test("deleteSession", async () => {
+    await adapter.deleteSession(session.sessionToken)
+    const dbSession = await db.session(session.sessionToken)
+    expect(dbSession).toBeNull()
+  })
+
+  // These are optional for custom adapters, but we require them for the official adapters
+
+  test("Verification Token methods exist", () => {
+    const requiredMethods = ["createVerificationToken", "useVerificationToken"]
+    requiredMethods.forEach((method) => {
+      expect(adapter).toHaveProperty(method)
+    })
+  })
+
+  test("createVerificationToken", async () => {
+    const identifier = "info@example.com"
+    const token = randomUUID()
+    const hashedToken = hashToken(token)
+
+    const verificationToken = {
+      token: hashedToken,
+      identifier,
+      expires: FIFTEEN_MINUTES_FROM_NOW,
+    }
+    await adapter.createVerificationToken?.(verificationToken)
+
+    const dbVerificationToken = await db.verificationToken({
+      token: hashedToken,
+      identifier,
     })
 
-    test("deleteUser", async () => {
-      let dbUser = await db.user(user.id)
-      expect(dbUser).toEqual(user)
+    expect(dbVerificationToken).toEqual(verificationToken)
+  })
 
-      await adapter.deleteUser?.(user.id)
-      dbUser = await db.user(user.id)
-      expect(dbUser).toBeNull()
+  test("useVerificationToken", async () => {
+    const identifier = "info@example.com"
+    const token = randomUUID()
+    const hashedToken = hashToken(token)
+    const verificationToken = {
+      token: hashedToken,
+      identifier,
+      expires: FIFTEEN_MINUTES_FROM_NOW,
+    }
+    await adapter.createVerificationToken?.(verificationToken)
 
-      const dbSession = await db.session(session.sessionToken)
-      expect(dbSession).toBeNull()
-
-      const dbAccount = await db.account({
-        provider: account.provider,
-        providerAccountId: account.providerAccountId,
-      })
-      expect(dbAccount).toBeNull()
+    const dbVerificationToken1 = await adapter.useVerificationToken?.({
+      identifier,
+      token: hashedToken,
     })
+
+    if (!dbVerificationToken1) {
+      throw new Error("Verification Token was not found, but it should exist")
+    }
+
+    expect(dbVerificationToken1).toEqual(verificationToken)
+
+    const dbVerificationToken2 = await adapter.useVerificationToken?.({
+      identifier,
+      token: hashedToken,
+    })
+
+    expect(dbVerificationToken2).toBeNull()
+  })
+
+  // Future methods
+  // These methods are not yet invoked in the core, but built-in adapters must implement them
+  test("Future methods exist", () => {
+    const requiredMethods = ["unlinkAccount", "deleteUser"]
+    requiredMethods.forEach((method) => {
+      expect(adapter).toHaveProperty(method)
+    })
+  })
+
+  test("unlinkAccount", async () => {
+    let dbAccount = await db.account({
+      provider: account.provider,
+      providerAccountId: account.providerAccountId,
+    })
+    expect(dbAccount).toEqual({ ...account, id: dbAccount.id })
+
+    await adapter.unlinkAccount?.({
+      provider: account.provider,
+      providerAccountId: account.providerAccountId,
+    })
+    dbAccount = await db.account({
+      provider: account.provider,
+      providerAccountId: account.providerAccountId,
+    })
+    expect(dbAccount).toBeNull()
+
+    // Re-add account so we can test `deleteUser` under
+    await adapter.linkAccount(account)
+  })
+
+  test("deleteUser", async () => {
+    let dbUser = await db.user(user.id)
+    expect(dbUser).toEqual(user)
+
+    await adapter.deleteUser?.(user.id)
+    dbUser = await db.user(user.id)
+    expect(dbUser).toBeNull()
+
+    const dbSession = await db.session(session.sessionToken)
+    expect(dbSession).toBeNull()
+
+    const dbAccount = await db.account({
+      provider: account.provider,
+      providerAccountId: account.providerAccountId,
+    })
+    expect(dbAccount).toBeNull()
   })
 }
 
--- packages/fauna/tests/index.test.ts
+++ packages/fauna/tests/index.test.ts
@@ -1,114 +1,44 @@
-// eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
-// @ts-ignore
-import { FaunaAdapter } from "../src"
+import {
+  collections,
+  FaunaAdapter,
+  indexes,
+  query,
+  toAccount,
+  toSession,
+  toUser,
+  toVerificationToken,
+} from "../src"
 import { runBasicTests } from "../../../basic-tests"
-import * as Fauna from "faunadb"
+import { query as q, Client as FaunaClient } from "faunadb"
 
-const client = new Fauna.Client({
+const f = new FaunaClient({
   secret: "secret",
   scheme: "http",
   domain: "localhost",
   port: 8443,
 })
 
-const adapter = FaunaAdapter({ faunaClient: client })
-
-const q = Fauna.query
-
-/**
- * FaunaDB throws an error when something is not found in the db,
- * but the basic tests expect it to be `null`.
- */
-async function returnNullIfError(fql: any) {
-  try {
-    return await client.query(fql)
-  } catch (error) {
-    if (error.name === "NotFound") return null
-    throw error
-  }
-}
 runBasicTests({
-  adapter,
+  adapter: FaunaAdapter(f),
   db: {
+    disconnect: async () => await f.close({ force: true }),
     async user(id) {
-      const response = await returnNullIfError(
-        q.Get(q.Ref(q.Collection("users"), id))
-      )
-      // @ts-expect-error
-      const user = response.data
-      user.createdAt = new Date(user.createdAt.value)
-      user.updatedAt = new Date(user.updatedAt.value)
-      // @ts-expect-error
-      user.id = response.ref.id
-      return user
+      const ref = q.Ref(q.Collection(collections.User), id)
+      return await query(f, q.Get(ref), toUser)
     },
     async session(sessionToken) {
-      return await returnNullIfError(
-        q.Get(q.Match(q.Index("session_by_token"), sessionToken))
-      )
+      const ref = q.Match(q.Index(indexes.Session), sessionToken)
+      return await query(f, q.Get(ref), toSession)
     },
-    async expireSession(sessionToken, expires) {
-      await client.query(
-        q.Update(
-          q.Select(
-            ["ref"],
-            q.Get(q.Match(q.Index("session_by_token"), sessionToken))
-          ),
-          { data: { expires: q.Time(expires.toISOString()) } }
-        )
-      )
+    async account({ provider, providerAccountId }) {
+      const key = [provider, providerAccountId]
+      const ref = q.Match(q.Index(indexes.UserByAccount), key)
+      return await query(f, q.Get(ref), toAccount)
     },
-    async account(providerId, providerAccountId) {
-      const response = await returnNullIfError(
-        q.Get(
-          q.Match(q.Index("account_by_provider_account_id"), [
-            providerId,
-            providerAccountId,
-          ])
-        )
-      )
-
-      if (!response) {
-        return null
-      }
-
-      // @ts-expect-error
-      const account = response.data
-      account.createdAt = new Date(account.createdAt.value)
-      account.updatedAt = new Date(account.updatedAt.value)
-      account.accessTokenExpires = null
-
-      return account
-    },
-    async verificationRequest(identifier, hashedToken) {
-      const response: any = await returnNullIfError(
-        q.Let(
-          {
-            ref: q.Match(
-              q.Index("verification_request_by_token_and_identifier"),
-              [hashedToken, identifier]
-            ),
-          },
-          q.If(
-            q.Exists(q.Var("ref")),
-            {
-              ref: q.Var("ref"),
-              request: q.Select("data", q.Get(q.Var("ref"))),
-            },
-            null
-          )
-        )
-      )
-      if (response) {
-        const { request: verificationRequest } = response
-        return {
-          ...verificationRequest,
-          expires: new Date(verificationRequest.expires.value),
-          createdAt: new Date(verificationRequest.createdAt.value),
-          updatedAt: new Date(verificationRequest.updatedAt.value),
-        }
-      }
-      return null
+    async verificationToken({ identifier, token }) {
+      const key = [identifier, token]
+      const ref = q.Match(q.Index(indexes.VerificationToken), key)
+      return await query(f, q.Get(ref), toVerificationToken)
     },
   },
 })
