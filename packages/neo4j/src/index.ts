7cbf70a794fac7655cf5fb7ceb3f949a2dc0a102
Davey Brown <15802017+EarthlingDavey@users.noreply.github.com>
2021-08-23 14:19:40 +0100

    feat(neo4j): database wrap function

--- packages/neo4j/src/index.ts
+++ packages/neo4j/src/index.ts
@@ -1,85 +1,258 @@
 import neo4j from "neo4j-driver"
 import type { Adapter } from "next-auth/adapters"
+import { v4 as uuid } from "uuid"
 
-import {
-  createUser,
-  getUser,
-  getUserByEmail,
-  getUserByAccount,
-  updateUser,
-  deleteUser,
-} from "./user"
-
-import { linkAccount, unlinkAccount } from "./account"
-
-import {
-  createSession,
-  getSessionAndUser,
-  updateSession,
-  deleteSession,
-} from "./session"
-
-import {
-  createVerificationToken,
-  useVerificationToken,
-} from "./verificationToken"
+import { neo4jWrap } from "./utils"
 
 export function Neo4jAdapter(neo4jSession: typeof neo4j.Session): Adapter {
   return {
-    async createUser(data) {
-      return await createUser(neo4jSession, data)
+    // * * * * * * * * * * * * * * * *
+    // User
+    // * * * * * * * * * * * * * * * *
+
+    async createUser({ emailVerified, ...userData }) {
+      return await neo4jWrap(
+        neo4jSession,
+        ` CREATE (u:User)
+          SET
+            u += $userData
+            ${
+              undefined !== emailVerified
+                ? `, u.emailVerified = datetime($emailVerified)`
+                : ``
+            }
+          RETURN u`,
+        {
+          userData: { id: uuid(), ...userData },
+          emailVerified,
+        }
+      )
     },
 
     async getUser(id) {
-      return await getUser(neo4jSession, id)
+      return await neo4jWrap(
+        neo4jSession,
+        `MATCH (u:User { id: $id }) RETURN u`,
+        {
+          id,
+        },
+        {
+          tx: "read",
+        }
+      )
     },
 
     async getUserByEmail(email) {
-      return await getUserByEmail(neo4jSession, email)
+      return await neo4jWrap(
+        neo4jSession,
+        `MATCH (u:User { email: $email }) RETURN u`,
+        {
+          email,
+        },
+        {
+          tx: "read",
+        }
+      )
     },
 
     async getUserByAccount(provider_providerAccountId) {
-      return await getUserByAccount(neo4jSession, provider_providerAccountId)
+      return await neo4jWrap(
+        neo4jSession,
+        ` MATCH (u:User)-[:HAS_ACCOUNT]->(a:Account {
+            provider: $provider,
+            providerAccountId: $providerAccountId
+          })
+          RETURN u`,
+        {
+          ...provider_providerAccountId,
+        },
+        {
+          tx: "read",
+        }
+      )
     },
 
-    async updateUser(data) {
-      return await updateUser(neo4jSession, data)
+    async updateUser({ id, emailVerified, ...userData }) {
+      return await neo4jWrap(
+        neo4jSession,
+        ` MATCH (u:User { id: $id })
+          SET
+            u += $userData
+            ${
+              undefined !== emailVerified
+                ? `, u.emailVerified = datetime($emailVerified)`
+                : ``
+            }
+          RETURN u`,
+        {
+          id,
+          userData,
+          emailVerified,
+        }
+      )
     },
 
     async deleteUser(id) {
-      return await deleteUser(neo4jSession, id)
+      return await neo4jWrap(
+        neo4jSession,
+        ` MATCH (u:User { id: $id })
+          WITH u, properties(u) AS properties
+          DETACH DELETE u
+          RETURN { properties: properties} AS deletedUser`,
+        {
+          id,
+        }
+      )
     },
 
-    async linkAccount(data) {
-      return await linkAccount(neo4jSession, data)
+    // * * * * * * * * * * * * * * * *
+    // Account
+    // * * * * * * * * * * * * * * * *
+
+    async linkAccount({ userId, providerAccountId, provider, ...accountData }) {
+      return await neo4jWrap(
+        neo4jSession,
+        ` MATCH (u:User { id: $userId })
+          MERGE (a:Account { 
+            providerAccountId: $providerAccountId, 
+            provider: $provider 
+          }) 
+          ON CREATE SET 
+            a.id = $id
+          SET 
+            a += $accountData
+          MERGE (u)-[:HAS_ACCOUNT]->(a)
+          RETURN a { .*, userId: u.id } AS a`,
+        {
+          id: uuid(),
+          userId,
+          providerAccountId,
+          provider,
+          accountData,
+        }
+      )
     },
 
     async unlinkAccount(provider_providerAccountId) {
-      return await unlinkAccount(neo4jSession, provider_providerAccountId)
+      return await neo4jWrap(
+        neo4jSession,
+        ` MATCH (u:User)-[:HAS_ACCOUNT]->(a:Account { 
+            providerAccountId: $providerAccountId, 
+            provider: $provider 
+          })
+          WITH u, a, properties(a) AS properties
+          DETACH DELETE a 
+          RETURN { properties: properties { .*, userId: u.id }} AS deletedAccount`,
+        {
+          ...provider_providerAccountId,
+        }
+      )
     },
 
+    // * * * * * * * * * * * * * * * *
+    // Session
+    // * * * * * * * * * * * * * * * *
+
     async createSession(data) {
-      return await createSession(neo4jSession, data)
+      return await neo4jWrap(
+        neo4jSession,
+        ` MATCH (u:User { id: $userId })
+          CREATE (s:Session  {
+            id           : $id,
+            expires      : datetime($expires),
+            sessionToken : $sessionToken
+          })
+          CREATE (u)-[:HAS_SESSION]->(s)
+          RETURN { properties: s { .*, userId: u.id } } AS s`,
+        {
+          ...data,
+          id: uuid(),
+        }
+      )
     },
 
     async getSessionAndUser(sessionToken) {
-      return await getSessionAndUser(neo4jSession, sessionToken)
+      const result = await neo4jWrap(
+        neo4jSession,
+        `// Delete expired session
+        OPTIONAL MATCH (u:User)-[:HAS_SESSION]->(s:Session { sessionToken: $sessionToken })
+        WHERE s.expires <= datetime($now)
+        DETACH DELETE s
+        WITH count(s) AS c
+        // Valid session
+        MATCH (u:User)-[:HAS_SESSION]->(s:Session { sessionToken: $sessionToken })
+        RETURN { properties: s { .*, userId: u.id } } AS session, u AS user`,
+        {
+          sessionToken,
+          now: new Date().toISOString(),
+        },
+        {
+          returnFormat: ["session", "user"],
+        }
+      )
+      if (!result?.session || !result?.user) return null
+
+      return result
     },
 
-    async updateSession(data) {
-      return await updateSession(neo4jSession, data)
+    async updateSession({ sessionToken, expires, ...sessionData }) {
+      return await neo4jWrap(
+        neo4jSession,
+        `MATCH (u:User)-[:HAS_SESSION]->(s:Session { sessionToken: $sessionToken })
+        SET 
+          s += $sessionData
+          ${undefined !== expires ? `, s.expires = datetime($expires)` : ``}
+        RETURN { properties: s { .*, userId: u.id } } AS s`,
+        {
+          sessionToken,
+          sessionData,
+          expires,
+        }
+      )
     },
 
     async deleteSession(sessionToken) {
-      return await deleteSession(neo4jSession, sessionToken)
+      return await neo4jWrap(
+        neo4jSession,
+        ` MATCH (u:User)-[:HAS_SESSION]->(s:Session { sessionToken: $sessionToken })
+          WITH u, s, properties(s) AS properties
+          DETACH DELETE s
+          RETURN { properties: properties { .*, userId: u.id } } AS deletedSession`,
+        {
+          sessionToken,
+        }
+      )
     },
 
+    // * * * * * * * * * * * * * * * *
+    // VerificationToken
+    // * * * * * * * * * * * * * * * *
+
     async createVerificationToken(data) {
-      return await createVerificationToken(neo4jSession, data)
+      return await neo4jWrap(
+        neo4jSession,
+        ` MERGE (v:VerificationToken {
+            identifier: $identifier,
+            token: $token
+          })
+          SET v.expires = datetime($expires)
+          RETURN v`,
+        { ...data }
+      )
     },
 
     async useVerificationToken(data) {
-      return await useVerificationToken(neo4jSession, data)
+      return await neo4jWrap(
+        neo4jSession,
+        ` MATCH (v:VerificationToken {
+            identifier: $identifier,
+            token: $token 
+          })
+          WITH v, properties(v) AS properties
+          DETACH DELETE v
+          RETURN { properties: properties } AS deletedVerificationToken`,
+        { ...data }
+      )
     },
   }
 }
--- packages/neo4j/src/utils.ts
+++ packages/neo4j/src/utils.ts
@@ -18,4 +18,73 @@ const neo4jDateToJs = (value: typeof neo4j.DateTime | null) => {
   return new Date(value.toString())
 }
 
-export { neo4jToSafeNumber, neo4jDateToJs }
+const neo4jWrap = async (
+  session: typeof neo4j.Session,
+  statement: string,
+  values: any,
+  options?: any
+) => {
+  let result: any
+
+  const DATE_KEYS = ["emailVerified", "expires"]
+
+  // Transform date values from JS Date object to ISO strings.
+  DATE_KEYS.forEach((key: string) => {
+    if (values?.[key] instanceof Date) {
+      values[key] = values[key].toISOString()
+    }
+  })
+
+  // Database read or write transaction.
+  try {
+    if (options?.tx === "read") {
+      result = await session.readTransaction((tx) => tx.run(statement, values))
+    } else {
+      result = await session.writeTransaction((tx) => tx.run(statement, values))
+    }
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+
+  // Following are different ways to return the data
+
+  if (!options?.returnFormat) {
+    const properties = result?.records[0]?.get(0)?.properties
+
+    DATE_KEYS.forEach((key: string) => {
+      if (properties?.[key]) {
+        properties[key] = neo4jDateToJs(properties[key])
+      }
+    })
+
+    return properties || null
+  }
+
+  if (Array.isArray(options?.returnFormat)) {
+    const returnObject: any = {}
+    options?.returnFormat.forEach((returnKey: string) => {
+      returnObject[returnKey] =
+        result?.records[0]?.get(returnKey)?.properties || null
+
+      DATE_KEYS.forEach((key: string) => {
+        if (returnObject[returnKey]?.[key]) {
+          returnObject[returnKey][key] = neo4jDateToJs(
+            returnObject[returnKey][key]
+          )
+        }
+      })
+    })
+
+    return returnObject
+  }
+
+  // Return the database data without any transforms.
+  if (options?.returnFormat === "raw") {
+    return result
+  }
+
+  return null
+}
+
+export { neo4jToSafeNumber, neo4jDateToJs, neo4jWrap }
