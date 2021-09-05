f713057299fc21edfac513df226c67398bfbcee4
Balázs Orbán <info@balazsorban.com>
2021-09-05 13:02:47 +0200

    feat(mongodb): add standalone mongoDB adapter (#193)
    * feat(mongodb): add mongoDB adapter
    
    * docs(mongodb): fix import
    
    * fix(mongodb): do not use `this`
    
    * refactor(mongodb): copy connection handling from Next.js examples
    
    * refactor(mongodb): don't pass ObjectId as option
    
    * refactor(mongodb): convert `userId` to ObjectId
    
    * test: add comments

---
+++ packages/mongodb/tests/index.test.ts
@@ -0,0 +1,53 @@
+import { runBasicTests } from "../../../basic-tests"
+import { collections, format, MongoDBAdapter, _id } from "../src"
+import { MongoClient } from "mongodb"
+const client = new MongoClient("mongodb://localhost:27017")
+const databaseName = "test"
+const db = client.db(databaseName)
+
+runBasicTests({
+  adapter: MongoDBAdapter({ db }),
+  db: {
+    async connect() {
+      return await client.connect()
+    },
+    async disconnect() {
+      await client.db("test").dropDatabase()
+      await client.close()
+    },
+    async user(id) {
+      const user = await client
+        .db("test")
+        .collection(collections.Users)
+        .findOne({ _id: _id(id) })
+
+      if (!user) return null
+      return format.from(user)
+    },
+    async account(provider_providerAccountId) {
+      const account = await client
+        .db("test")
+        .collection(collections.Accounts)
+        .findOne(provider_providerAccountId)
+      if (!account) return null
+      return format.from(account)
+    },
+    async session(sessionToken) {
+      const session = await client
+        .db("test")
+        .collection(collections.Sessions)
+        .findOne({ sessionToken })
+      if (!session) return null
+      return format.from(session)
+    },
+    async verificationToken(identifier_token) {
+      const token = await client
+        .db("test")
+        .collection(collections.VerificationTokens)
+        .findOne(identifier_token)
+      if (!token) return null
+      delete token._id
+      return token
+    },
+  },
+})
