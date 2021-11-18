cf2bbd3065f8827b28c7215e05008313b89f9b58
Mathis Obadia <mathis.obadia@gmail.com>
2021-11-19 00:16:34 +0100

    fix(dynamo): change install path for dynamodb local

--- packages/dynamodb/jest-dynamodb-config.js
+++ packages/dynamodb/jest-dynamodb-config.js
@@ -33,4 +33,9 @@ module.exports = {
     // etc
   ],
   port: 8000,
+  installerConfig: {
+    installPath: "./dynamodblocal-bin",
+    downloadUrl:
+      "https://s3.eu-central-1.amazonaws.com/dynamodb-local-frankfurt/dynamodb_local_latest.tar.gz",
+  },
 }
