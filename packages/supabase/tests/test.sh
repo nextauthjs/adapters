840dab64f995b97bb74fd76c045196b0ece0f494
Balázs Orbán <info@balazsorban.com>
2021-11-23 14:40:08 +0100

    test(supabase): support Jest watch mode

--- packages/supabase/tests/test.sh
+++ packages/supabase/tests/test.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+JEST_WATCH=false
 npx supabase start
+
+# Is the watch flag passed to the script?
+while getopts w flag
+do
+    case "${flag}" in
+        w) JEST_WATCH=true;;
+        *) continue;;
+    esac
+done
 
 echo "Waiting 5 sec for db to start..." && sleep 5
 
@@ -14,9 +24,16 @@ sh -c 'psql -h localhost -U postgres' < "$(pwd)/supabase/schema.sql"
 
 echo "Waiting 1 sec before running tests..." && sleep 1
 
-# Always stop container, but exit with 1 when tests are failing
-if npx jest;then
-    npx supabase stop
+if $JEST_WATCH; then
+    # Run jest in watch mode
+    npx jest tests --watch
+    # Only stop the container after jest has been quit
+    docker stop "${CONTAINER_NAME}"
 else
-    npx supabase stop && exit 1
+    # Always stop container, but exit with 1 when tests are failing
+    if npx jest;then
+        npx supabase stop
+    else
+        npx supabase stop && exit 1
+    fi
 fi
