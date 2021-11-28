52f3e81bee385646ffbbac6e925e6dec92aacd46
Balázs Orbán <info@balazsorban.com>
2021-11-28 21:20:06 +0100

    refactor(mikro-orm): simplify source code

--- packages/mikro-orm/src/index.ts
+++ packages/mikro-orm/src/index.ts
@@ -1,173 +1,162 @@
-import type { EntityManager } from "@mikro-orm/core"
 import { wrap, RequestContext } from "@mikro-orm/core"
-
 import * as defaultModels from "./models"
-import type {
-  Adapter,
-  AdapterSession,
-  VerificationToken as AdapterVerificationToken,
-} from "next-auth/adapters"
-import type { DefaultAccount } from "next-auth"
 
-export const getEM = (emPromise?: Promise<EntityManager> | EntityManager) => {
-  const em = RequestContext.getEntityManager() ?? emPromise
-  if (!em)
-    throw new Error("Neither RequestContext, nor EntityManager provided.")
-  return em
+import type { EntityManager } from "@mikro-orm/core"
+import type { Adapter } from "next-auth/adapters"
+
+export * as defaultModels from "./models"
+
+function getEM(em?: Promise<EntityManager> | EntityManager) {
+  if (em) return em
+
+  const _em = RequestContext.getEntityManager()
+  if (_em) return _em
+
+  throw new Error("Neither RequestContext, nor EntityManager provided.")
 }
 
-export function MikroOrmAdapter<
-  TUserModel extends typeof defaultModels.User = typeof defaultModels.User,
-  TAccountModel extends typeof defaultModels.Account = typeof defaultModels.Account,
-  TSessionModel extends typeof defaultModels.Session = typeof defaultModels.Session,
-  TVerificationTokenModel extends typeof defaultModels.VerificationToken = typeof defaultModels.VerificationToken
->(
-  emPromise?: Promise<EntityManager> | EntityManager,
-  models?: {
-    User?: TUserModel
-    Account: TAccountModel
-    Session?: TSessionModel
-    VerificationToken?: TVerificationTokenModel
-  }
+export function MikroOrmAdapter(
+  entityManager?: Promise<EntityManager> | EntityManager,
+  options?: { models?: typeof defaultModels }
 ): Adapter {
-  const UserModel = models?.User ?? defaultModels.User
-  const AccountModel = models?.Account ?? defaultModels.Account
-  const SessionModel = models?.Session ?? defaultModels.Session
-  const VerificationTokenModel =
-    models?.VerificationToken ?? defaultModels.VerificationToken
+  const {
+    User: UserModel,
+    Account: AccountModel,
+    Session: SessionModel,
+    VerificationToken: VerificationTokenModel,
+  } = { ...defaultModels, ...options?.models }
 
+  const em = getEM(entityManager)
   return {
-    createUser: async (data) => {
-      const em = await getEM(emPromise)
+    /**
+     * Method used in testing. You won't need to call this in your app.
+     * @internal
+     */
+    // @ts-expect-error
+    async __disconnect() {
+      await (await em).getConnection().close()
+    },
+    async createUser(data) {
       const user = new UserModel({ ...data })
-      await em.persistAndFlush(user)
+      await (await em).persistAndFlush(user)
 
       return wrap(user).toObject()
     },
-    getUser: async (id) => {
-      const em = await getEM(emPromise)
-      const user = await em.findOne(UserModel, { id })
+    async getUser(id) {
+      const user = await (await em).findOne(UserModel, { id })
       if (!user) return null
 
       return wrap(user).toObject()
     },
-    getUserByEmail: async (email) => {
-      const em = await getEM(emPromise)
-      const user = await em.findOne(UserModel, { email })
+    async getUserByEmail(email) {
+      const user = await (await em).findOne(UserModel, { email })
       if (!user) return null
 
       return wrap(user).toObject()
     },
-    getUserByAccount: async (provider_providerAccountId) => {
-      const em = await getEM(emPromise)
-      const account = await em.findOne(AccountModel, {
+    async getUserByAccount(provider_providerAccountId) {
+      const account = await (
+        await em
+      ).findOne(AccountModel, {
         ...provider_providerAccountId,
       })
       if (!account) return null
-      const user = await em.findOne(UserModel, { id: account.userId })
+      const user = await (await em).findOne(UserModel, { id: account.userId })
 
       return wrap(user).toObject()
     },
-    updateUser: async (data) => {
-      const em = await getEM(emPromise)
-      const user = await em.findOne(UserModel, { id: data.id })
+    async updateUser(data) {
+      const user = await (await em).findOne(UserModel, { id: data.id })
       if (!user) throw new Error("User not found")
       wrap(user).assign(data, { mergeObjects: true })
-      await em.persistAndFlush(user)
+      await (await em).persistAndFlush(user)
 
       return wrap(user).toObject()
     },
-    deleteUser: async (id) => {
-      const em = await getEM(emPromise)
-      const user = await em.findOne(UserModel, { id })
+    async deleteUser(id) {
+      const user = await (await em).findOne(UserModel, { id })
       if (!user) return null
-      await em.removeAndFlush(user)
+      await (await em).removeAndFlush(user)
 
       return wrap(user).toObject()
     },
-    linkAccount: async (data) => {
-      const em = await getEM(emPromise)
-      const user = await em.findOne(UserModel, { id: data.userId })
+    async linkAccount(data) {
+      const user = await (await em).findOne(UserModel, { id: data.userId })
       if (!user) throw new Error("User not found")
       const account = new AccountModel(data)
       user.accounts.add(account)
-      await em.persistAndFlush(user)
+      await (await em).persistAndFlush(user)
 
-      return wrap(account).toObject() as DefaultAccount
+      return wrap(account).toObject()
     },
-    unlinkAccount: async (provider_providerAccountId) => {
-      const em = await getEM(emPromise)
-      const account = await em.findOne(AccountModel, {
+    async unlinkAccount(provider_providerAccountId) {
+      const account = await (
+        await em
+      ).findOne(AccountModel, {
         ...provider_providerAccountId,
       })
       if (!account) throw new Error("Account not found")
-      await em.removeAndFlush(account)
+      await (await em).removeAndFlush(account)
 
-      return wrap(account).toObject() as DefaultAccount
+      return wrap(account).toObject()
     },
-    getSessionAndUser: async (sessionToken) => {
-      const em = await getEM(emPromise)
-      const session = await em.findOne(
-        SessionModel,
-        { sessionToken },
-        { populate: ["user"] }
-      )
+    async getSessionAndUser(sessionToken) {
+      const session = await (
+        await em
+      ).findOne(SessionModel, { sessionToken }, { populate: ["user"] })
       if (!session || !session.user) return null
 
       return {
         user: wrap(session.user).toObject(),
-        session: wrap(session).toObject() as AdapterSession,
+        session: wrap(session).toObject(),
       }
     },
-    createSession: async (data) => {
-      const em = await getEM(emPromise)
-      const user = await em.findOne(UserModel, { id: data.userId })
+    async createSession(data) {
+      const user = await (await em).findOne(UserModel, { id: data.userId })
       if (!user) throw new Error("User not found")
       const session = new SessionModel(data)
       user.sessions.add(session)
-      await em.persistAndFlush(user)
+      await (await em).persistAndFlush(user)
 
-      return wrap(session).toObject() as AdapterSession
+      return wrap(session).toObject()
     },
-    updateSession: async (data) => {
-      const em = await getEM(emPromise)
-      const session = await em.findOne(SessionModel, {
+    async updateSession(data) {
+      const session = await (
+        await em
+      ).findOne(SessionModel, {
         sessionToken: data.sessionToken,
       })
       wrap(session).assign(data)
       if (!session) throw new Error("Session not found")
-      await em.persistAndFlush(session)
+      await (await em).persistAndFlush(session)
 
-      return wrap(session).toObject() as AdapterSession
+      return wrap(session).toObject()
     },
-    deleteSession: async (sessionToken) => {
-      const em = await getEM(emPromise)
-      const session = await em.findOne(SessionModel, {
+    async deleteSession(sessionToken) {
+      const session = await (
+        await em
+      ).findOne(SessionModel, {
         sessionToken,
       })
       if (!session) return null
-      await em.removeAndFlush(session)
+      await (await em).removeAndFlush(session)
 
-      return wrap(session).toObject() as AdapterSession
+      return wrap(session).toObject()
     },
-    createVerificationToken: async (data) => {
-      const em = await getEM(emPromise)
+    async createVerificationToken(data) {
       const verificationToken = new VerificationTokenModel(data)
-      await em.persistAndFlush(verificationToken)
+      await (await em).persistAndFlush(verificationToken)
 
-      return wrap(verificationToken).toObject() as AdapterVerificationToken
+      return wrap(verificationToken).toObject()
     },
-    useVerificationToken: async (params) => {
-      const em = await getEM(emPromise)
-      const verificationToken = await em
+    async useVerificationToken(params) {
+      const verificationToken = await (await em)
         .getRepository(VerificationTokenModel)
         .findOne(params)
       if (!verificationToken) return null
-      await em.removeAndFlush(verificationToken)
+      await (await em).removeAndFlush(verificationToken)
 
-      return wrap(verificationToken).toObject() as AdapterVerificationToken
+      return wrap(verificationToken).toObject()
     },
   }
 }
-
-export * as defaultModels from "./models"
