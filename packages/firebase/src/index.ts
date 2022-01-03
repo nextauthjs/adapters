907e715523d9d8d3cc36de144589e3600671b51c
Srijan Sharma <srijans38@gmail.com>
2022-01-04 04:09:04 +0530

    feat: firebase auth

--- packages/firebase/src/index.ts
+++ packages/firebase/src/index.ts
@@ -1,8 +1,9 @@
-import type { Adapter, AdapterSession, AdapterUser } from "next-auth/adapters"
+import type { Adapter, AdapterUser } from "next-auth/adapters"
 
 import { Firestore, FirestoreDataConverter } from "firebase-admin/firestore"
+import type { Auth } from "firebase-admin/auth"
 
-export const format: FirestoreDataConverter<any> = {
+export const formatFirestore: FirestoreDataConverter<any> = {
   toFirestore(object) {
     const newObjectobject: any = {}
     for (const key in object) {
@@ -24,6 +25,30 @@ export const format: FirestoreDataConverter<any> = {
   },
 }
 
+export const formatUserRecord = {
+  toUserRecord(user: Partial<AdapterUser>): any {
+    const userRecord: any = {}
+    for (const key in user) {
+      const value = user[key]
+      if (value === undefined) continue
+      if (key === "name") {
+        userRecord.displayName = value
+        continue
+      }
+      if (key === "image") {
+        userRecord.photoURL = value
+        continue
+      }
+      if (key === "emailVerified") {
+        userRecord.emailVerified = !!value
+        continue
+      }
+      userRecord[key] = value
+    }
+    return userRecord
+  },
+}
+
 export const collections = {
   Users: "users",
   Sessions: "sessions",
@@ -33,32 +58,38 @@ export const collections = {
 
 export interface FirebaseClient {
   db: Firestore
+  auth: Auth
 }
 
 export function FirebaseAdapter(client: FirebaseClient): Adapter {
-  const { db } = client
+  const { db, auth } = client
 
-  const Users = db.collection(collections.Users).withConverter(format)
-  const Sessions = db.collection(collections.Sessions).withConverter(format)
-  const Accounts = db.collection(collections.Accounts).withConverter(format)
+  const Users = db.collection(collections.Users).withConverter(formatFirestore)
+  const Sessions = db
+    .collection(collections.Sessions)
+    .withConverter(formatFirestore)
+  const Accounts = db
+    .collection(collections.Accounts)
+    .withConverter(formatFirestore)
   const VerificationTokens = db
     .collection(collections.VerificationTokens)
-    .withConverter(format)
+    .withConverter(formatFirestore)
 
   return {
     async createUser(user) {
-      const { id } = await Users.add(user)
-      return { ...(user as any), id }
+      const newUser = await auth.createUser(formatUserRecord.toUserRecord(user))
+      await Users.doc(newUser.uid).set(user)
+      return { ...(user as any), id: newUser.uid }
     },
     async getUser(id) {
       const user = await Users.doc(id).get()
       if (!user.exists) return null
-      return user.data() as AdapterUser
+      return user.data()
     },
     async getUserByEmail(email) {
       const user = await Users.where("email", "==", email).limit(1).get()
       if (user.empty) return null
-      return user.docs[0].data() as AdapterUser
+      return user.docs[0].data()
     },
     async getUserByAccount({ provider, providerAccountId }) {
       const account = await Accounts.where("provider", "==", provider)
@@ -68,15 +99,20 @@ export function FirebaseAdapter(client: FirebaseClient): Adapter {
       if (account.empty) return null
       const user = await Users.doc(account.docs[0].data().userId).get()
       if (!user.exists) return null
-      return user.data() as AdapterUser
+      return user.data()
     },
     async updateUser(user) {
+      await auth.updateUser(
+        user.id as string,
+        formatUserRecord.toUserRecord(user)
+      )
       const userRef = Users.doc(user.id as string)
       await userRef.update(user)
       const userRes = await userRef.get()
-      return userRes.data() as AdapterUser
+      return userRes.data()
     },
     async deleteUser(userId) {
+      await auth.deleteUser(userId)
       const accountsQuery = Accounts.where("userId", "==", userId)
       const sessionsQuery = Sessions.where("userId", "==", userId)
 
@@ -119,8 +155,8 @@ export function FirebaseAdapter(client: FirebaseClient): Adapter {
       const user = await Users.doc(session.userId).get()
       if (!user.exists) return null
       return {
-        session: session as AdapterSession,
-        user: user.data() as AdapterUser,
+        session,
+        user: user.data(),
       }
     },
     async updateSession(partialSession) {
