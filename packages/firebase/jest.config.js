09f41e4be2ccbfd08d3b142c7dcdceb15c33f09d
Srijan Sharma <srijans38@gmail.com>
2022-01-04 02:55:47 +0530

    fix: jest config for firebase-admin

--- packages/firebase/jest.config.js
+++ packages/firebase/jest.config.js
@@ -1,1 +1,6 @@
-module.exports = require("../../jest.config")
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  modulePathIgnorePatterns: ["dist/", "node_modules/"],
+  resolver: "jest-node-exports-resolver",
+}
